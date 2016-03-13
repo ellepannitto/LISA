c1d0b3a7b27d329441c233c6cee049f98c3a4333
Ludovica <ellepannitto@gmail.com>
2016-03-13 12:02:27 +0100

    versione 0.2

--- Hasher.py
+++ Hasher.py
@@ -2,12 +2,27 @@
 #coding: utf-8
 
 class Hasher:
-	
+	"""
+	Gli oggetti di questa classe vengono usati per generare dei codici univoci interi (hash) a partire da qualsiasi variabile, tipicamente stringhe
+	 È gatantito che oggetti uguali abbiano lo stesso hash.
+	 È garantito che oggetti diversi abbiano hash diverso
+	"""
 	def __init__ (self):
+		"""
+		Costruttore di default
+		"""
 		self.next_hash = 0
 		self.dizionario_hash = {}
 	
 	def hash (self, s):
+		"""
+		Restituisce l'hash di una variabile
+		 Parametri:
+		  s: la variabile della quale calcolare l'hash
+		 
+		 Valore restituito:
+		  l'hash di s
+		"""
 		if s in self.dizionario_hash:
 			ret = self.dizionario_hash[s]
 		else:
@@ -16,3 +31,19 @@ class Hasher:
 		return self.dizionario_hash[s]
 
 	
+def test_hash ():
+	"""
+	esegue alcuni test per verificare il funzionamento della classe Hasher
+	"""
+	a = "Ciao Mondo"
+	b = "Ciao Mondo"
+	c = "Ciamo oondo"
+	h = Hasher ()
+	assert h.hash (a) == h.hash (b), "[Hasher] ERROR: oggetti uguali devono avere lo stesso hash"
+	assert h.hash (a) == h.hash (a), "[Hasher] ERROR: oggetti uguali devono avere lo stesso hash"
+	assert h.hash (a) != h.hash (c), "[Hasher] ERROR: oggetti diversi devono avere diversi hash"
+	
+	print "All tests successfully passed"
+
+#uncomment to perform tests
+#test_hash ()
